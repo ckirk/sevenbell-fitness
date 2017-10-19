f029fb0f4f2d445a21f842a036c83acc6841931f
Chris <ckirkinis@gmail.com>
2017-10-19 19:46:34 -0400

    Finished footer

--- app/components/LandingPage.js
+++ app/components/LandingPage.js
@@ -294,7 +294,43 @@ class LandingPage extends React.Component {
 
         {/* FOOTER */}
         <div id="footer">
+          <div className="logo">
+            <Link to='/' >
+              <img src='../images/logo.png' />
+            </Link>
+          </div>
+          <div className="info">
+            <div className="address">
 
+                <p>640 Dean St.</p>
+                <p>Brooklyn, NY 11238</p>
+                <a href="tel:718-857-2355"> (718) 857-2355</a>
+
+            </div>
+            <div className="hours">
+
+                <p>Monday-Thursday: 5:30am-10pm</p>
+                <p>Friday: 5:30am-8pm</p>
+                <p>Saturday & Sunday: 9am-8pm</p>
+
+            </div>
+          </div>
+          <div className="right">
+            <div className="socials">
+              <a href='#'>
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+              <a href='#'>
+                <i className="fa fa-facebook-square" aria-hidden="true"></i>
+              </a>
+              <a href='#'>
+                <i className="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div className="copyright">
+              Copyright All Rights Reserved © 2017 | SEVENBELLFITNESS
+            </div>
+          </div>
         </div>
       </div>
     );
