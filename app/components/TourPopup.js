27f7401f8f2300189548d20a41d450a17a5119d1
Chris <ckirkinis@gmail.com>
2020-02-17 12:29:06 -0500

    added second email to FormSpree

--- app/components/TourPopup.js
+++ app/components/TourPopup.js
@@ -99,7 +99,7 @@ class TourPopup extends React.Component {
             <input className='inline' type="text" name="email" onChange={this.handleInputChange} placeholder="Email" />
             <input className='inline' type="text" name="phone" onChange={this.handleInputChange} placeholder="Phone Number" />
             <input type="hidden" name="_subject" value={"New Membership Inquiry: " + this.state.name} />
-            {/* <input type="hidden" name="_cc" value="jesse.barocas@gmail.com" /> */}
+            <input type="hidden" name="_cc" value="solutions@sevenbellfitness.com" />
 
             <label>
               <input
